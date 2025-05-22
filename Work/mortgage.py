26dac57b3acf442876de53866a774a63bfcbd76c
Jafir Kluis <97438399+stayhomewelldone@users.noreply.github.com>
2025-05-22 22:11:48 +0200

    Exercise 1.17 finished.
    Exercise about mortgages and rate.

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,21 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+months = 0
+extra_payment = 1000
+
+
+while principal > 0:
+    if months < 12:
+        principal -= 1000
+        total_paid+=1000
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+    months+=1
+
+print(f'Number of months required: {months}. Total paid: {round(total_paid,2)}' )
