f056cc0a61ef868474a91016a7b6c7e756955cb8
Jafir Kluis <97438399+stayhomewelldone@users.noreply.github.com>
2025-05-26 09:35:35 +0200

    Exercise 2.4 finished

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,23 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+import sys
+portfolio = []
+def read_portfolio(filename):
+    'Reads the content of a file'
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            print(row)
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+    return portfolio
+
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+cost = read_portfolio('Data/portfolio.csv')
