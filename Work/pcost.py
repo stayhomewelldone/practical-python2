b875c1ecfd471b057b8e9d63b82e8824173990ac
Jafir Kluis <97438399+stayhomewelldone@users.noreply.github.com>
2025-05-26 09:06:43 +0200

    Finished chapter 1.

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,14 +1,26 @@
 # pcost.py
 #
 # Exercise 1.27
-total_cost = 0
-with open('Data/portfolio.csv', 'rt') as f:
-    headers = next(f)
-    for line in f:
-        row = line.split(',')
-        total_cost += int(row[1]) * float(row[2]) 
-        
+import csv
+import sys
+def portfolio_cost(filename):
+    'Calculates the whole value of the portfolio'
+    total_cost = 0
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for line in rows:
+            try:
+                total_cost += int(line[1]) * float(line[2])
+            except ValueError:
+                print("Couldn't parse", line)
+        return float(total_cost)
 
 
-print(f'Total cost {total_cost}')
-        
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+cost = portfolio_cost('Data/portfolio.csv')
+print(f'Total cost {cost}')
+
