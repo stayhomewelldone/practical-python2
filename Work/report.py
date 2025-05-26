63e077e8eec41de08600fb8bf6dc8f1e1e880711
Jafir Kluis <97438399+stayhomewelldone@users.noreply.github.com>
2025-05-26 13:00:06 +0200

    Finished 2.10

--- Work/report.py
+++ Work/report.py
@@ -54,3 +54,6 @@ else:
 portfolio = read_portfolio(portfile)
 prices = read_prices(pricesfile)
 print(calculate_portfolio(portfolio, prices))
+report = make_report(portfolio,prices)
+for name, shares, price, change in report:
+    print(f'{name:>10s} {shares:>10d} {price:>10.2f} {change:>10.2f}')
