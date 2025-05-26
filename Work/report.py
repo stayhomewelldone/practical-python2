4d8e5b9d9ccdd52bbcaeb78616d6f84be8f51c04
Jafir Kluis <97438399+stayhomewelldone@users.noreply.github.com>
2025-05-26 10:36:10 +0200

    Chapter 2.2 finished.

--- Work/report.py
+++ Work/report.py
@@ -4,6 +4,8 @@
 import csv
 import sys
 portfolio = []
+prices = {}
+
 def read_portfolio(filename):
     'Reads the content of a file'
     with open(filename, 'rt') as f:
@@ -14,9 +16,35 @@ def read_portfolio(filename):
             portfolio.append(holding)
     return portfolio
 
+def read_prices(file):
+    'Reads the prices of a file'
+    with open(file, 'r') as f:
+        rows = csv.reader(f)
+        try:
+            for number, row in enumerate(rows):
+                prices[row[0]] = row[1]
+        except IndexError:
+            print('Indexerror on line',number)
+    return prices
 
-if len(sys.argv) == 2:
-    filename = sys.argv[1]
+def calculate_portfolio(port, prices):
+    'Calculate the current value of the portfolio'
+    portvalue = 0.0
+    pricevalue = 0.0
+    for stocks in port:
+        portvalue += stocks['shares'] * stocks['price']
+        pricevalue += float(prices[stocks['name']]) * stocks['shares']
+    return portvalue, pricevalue
+
+if len(sys.argv) == 3:
+    
+    portfile = sys.argv[1]
+    pricesfile = sys.argv[2]
 else:
-    filename = 'Data/portfolio.csv'
-cost = read_portfolio('Data/portfolio.csv')
+    portfile = 'Data/portfolio.csv'
+    pricesfile = 'Data/prices.csv'
+portfolio = read_portfolio(portfile)
+prices = read_prices(pricesfile)
+old_value, pricevalue = calculate_portfolio(portfolio, prices)
+current_value = pricevalue - old_value
+print(current_value)
